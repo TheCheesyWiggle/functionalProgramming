2738bc6034d0f98e9489e3926ede0cfd0a97fb90
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-12-06 15:35:43 +0000

    no worky

--- p3 - Coursework.pl
+++ p3 - Coursework.pl
@@ -1,18 +1,66 @@
+main():-
+    member(0,[1,2,3,0,5,6]).
+
 generator3(N):-
-    between(1000, 1000000, N),
+    between(10000, 1000000, N),
     X is integer(sqrt(N)),
     N is X * X.
 
-tester3():-
+tester3(N):-
+    % Convert the number to a list of digits, Ensure all digits are different
+    digits(N,[N1, N2, N3, N4, N5]),
+    X1>0, X2>0, X3>0, X4>0, N5>0,
+    nodups([N1, N2, N3, N4, N5]),
+    % Last digit is equal to the number of digits
     
+    % Last-but-one digit is odd
     
+    % One of the digits is zero
+    member(0,[N1, N2, N3, N4, N5]).
+    % Second, third, and last-but-one digits are exact multiples of the first digit
+
+digits(N,[N]) :-
+    N < 10.
+
+digits(N, W):-
+    N >= 10,
+    div_mod(N, 10, D, M),
+    digits(D, R),
+    append(R, [M], W).
+
+digits(N, W):-
+    N >= 100,
+    div_mod(N, 100, D, M),
+    digits(D, R),
+    append(R, [M], W).
+
+    
+nub([],[]).
+nub([X|XS], [X|W]):-
+    \+ member(X, XS),
+    nub(XS, W).
+
+nub([X|XS],W):-
+    member(X, XS),
+    nub( XS, W).
+
+length_list( [] , 0 ).
+length_list( [ _ | T ] , N ):-
+  length_list( T , W ),
+  N is W + 1.
+    
+nodups([],[]).
+nodups(X):-
+    length_list(X) \= nub(X).
+    
+   
 x_generator3(N) :-
   	x_generator3_loop(
         [ 1024 , 9409 , 23716 , 51529
     	, 123904 , 185761 , 868624 , 962361
     	, 982081 , 1000000 ] , 0 , N ).
 
-x_generator3_loop( [] , C , C ) .
+x_generator3_loop([] , C , C ) .
 x_generator3_loop([T|TS], C, N):-
   	generator3(T),
   	C1 is C+1,

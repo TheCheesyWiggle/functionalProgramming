c7438f4c86a0962a34f633c82f21465bd6674614
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-12-10 16:03:36 +0000

    TRYING TO GENERATE

--- p4 - Coursework.pl
+++ p4 - Coursework.pl
@@ -1,48 +1,110 @@
 main(N):-
     x_generator4(N).
 
-generator4(XS) :- 
-    permutation([0,1,2,3,4,5,6,7,8,9], XS), 
-    is_valid_prime_set(XS).
+generator4(X) :-
+    X1 = [0,1,2,3,4,5,6,7,8,9],
+    % generate combinations up to 4 digits
+    combosOfSize([1,2,3,4,5,6,7,8,9,0],2,X2),
+    combosOfSize([1,2,3,4,5,6,7,8,9,0],2,X3),
+    combosOfSize([1,2,3,4,5,6,7,8,9,0],2,X4),
+    % generate permutations (not necessary to gen for X1)
+    perm(X2, PX2),
+    perm(X3, PX3),
+    perm(X4, PX4),
+    % Remove leading zeros
+    leadingZeros(X1,ZerosX1),
+    leadingZeros(PX2,ZerosX2),
+    leadingZeros(PX3,ZerosX3),
+    leadingZeros(PX4,ZerosX4),
+    flatten([ZerosX1, ZerosX2, ZerosX3, ZerosX4], X),
+    % Filter primes
+    validPrimeSet(X).
 
-is_valid_prime_set([]).
-is_valid_prime_set([H|T]):- 
+validPrimeSet([]).
+validPrimeSet([H|T]):- 
     list_to_number(H, Num), 
-    is_prime(Num), 
-    is_valid_prime_set(T).
+    prime(Num), 
+    validPrimeSet(T).
 
 list_to_number([], 0).
 list_to_number([D|Digits], Num):- 
     list_to_number(Digits, RestNum), Num is D + RestNum * 10.
 
-is_prime(2).
-is_prime(3).
-is_prime(P):- 
-    P > 3, P mod 2 =\= 0,
-    \+ has_factor(P, 3).
+leadingZeros([H|T], [H|T_no_zeros]):-
+    H \= 0,
+    T_no_zeros = T.
+leadingZeros([0|T], T_no_zeros):-
+    (T, T_no_zeros).
+    
 
-has_factor(N, F):- N mod F =:= 0.
-has_factor(N, F):- 
+% Works
+combos([], []).
+combos([Head|Tail], [Head|Combo]) :-
+    combos(Tail, Combo).
+combos([_|Tail], Combo) :-
+    combos(Tail, Combo).
+
+% Works
+perm([], []).
+perm(List, [X|Perm]) :-
+    select(X, List, Rest),
+    perm(Rest, Perm).
+
+% Works
+%concat([], List2, List3, List4, Result) :-
+%    concat(List2, List3, List4, Result).
+%concat([Head|Tail1], List2, List3, List4, [Head|Result]) :-
+%    concat(Tail1, List2, List3, List4, Result).
+
+%concat([], List3, List4, Result) :-
+%    concat(List3, List4, Result).
+%concat([Head|Tail1], List3, List4, [Head|Result]) :-
+%    concat(Tail1, List3, List4, Result).
+
+%concat([], List2, Result) :- Result = List2.
+%concat([Head|Tail1], List2, [Head|Result]) :-
+%    concat(Tail1, List2, Result).
+
+% Works
+combosOfSize(_, 0, []).
+combosOfSize(List, Size, Combo) :-
+    Size > 0,
+    Size =< 4, 
+    append(_, [Head|Tail], List),
+    Size1 is Size - 1,
+    combosOfSize(Tail, Size1, SubCombo),
+    Combo = [Head|SubCombo].
+
+% Works
+prime(2).
+prime(3).
+prime(P):- 
+    P > 3, P mod 2 =\= 0,
+    \+ hasFactor(P, 3).
+% Works
+hasFactor(N, F):- N mod F =:= 0.
+hasFactor(N, F):- 
     F * F < N, 
     F2 is F + 2, 
-    has_factor(N, F2).
+    hasFactor(N, F2).
 
 tester4(XS):- 
     discard_smallest_prime(XS, Discarded), 
     descending_order(Discarded, Desc), 
-    is_valid_cube_set(Desc).
+    valiSet(Desc).
 
 discard_smallest_prime([[_|T]|Rest], [T|Rest]).
 descending_order(List, Desc):- 
     flatten(List, Flat), 
     reverse(Flat, Desc).
 
-is_valid_cube_set([]).
-is_valid_cube_set([H|T]):- 
-    is_cube(H), 
-    is_valid_cube_set(T).
+validSet([]).
+validSet([H|T]):- 
+    isCube(H), 
+    validSet(T).
 
-is_cube(N):- 
+% Works
+isCube(N):- 
     N >= 0, 
     X is round(N^(1/3)), 
     N =:= X*X*X.

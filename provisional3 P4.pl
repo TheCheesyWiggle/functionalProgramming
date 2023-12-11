5eee29e01a68e0fd97275a64af434097cf3075a8
Finn van Montfort <74103207+TheCheesyWiggle@users.noreply.github.com>
2023-12-11 16:14:30 +0000

    Create provisional3 P4.pl

---
+++ provisional3 P4.pl
@@ -0,0 +1,89 @@
+main(N):-
+    x_tester4(N).
+
+generator4(N):-
+    D = [1,2,3,4,5,6,7,8,9,0],
+    % Generate combinations from size 1 to size 4.
+    combos(D,C),
+    % Generate permutations of these combinations.
+    perms(C,N).
+    % Collate all into one list.
+    % Remove leading zeros.
+    % Filter for primes.
+    % Check only one of each digit is used.
+
+
+% Check if a number is prime
+is_prime(2).
+is_prime(3).
+is_prime(N) :-
+    N > 3,
+    N mod 2 =\= 0,
+    \+ has_factor(N, 3).
+
+has_factor(N, Factor) :-
+    N mod Factor =:= 0.
+has_factor(N, Factor) :-
+    Factor * Factor < N,
+    NextFactor is Factor + 2,
+    has_factor(N, NextFactor).
+
+% Works
+combos([], []).
+combos([Head|Tail], [Head|Combo]) :-
+    combos(Tail, Combo),
+    length([Head|Combo], N), 
+    N =< 4.
+combos([_|Tail], Combo) :-
+    combos(Tail, Combo).
+
+% Works
+perms([], []).
+perms([X],[X]).
+perms(List, [X|Perm]) :-
+    select(X, List, Rest),
+    perms(Rest, Perm).
+
+
+    
+x_generator4(N):-
+	x_generator4_loop(
+        [ [[9 ,6 ,7] ,[4 ,0 ,1] ,[2 ,8 ,3] ,[5]]
+        , [[9 ,8 ,3] ,[6 ,0 ,1] ,[5] ,[4 ,7] ,[2]]
+        , [[9 ,8 ,3] ,[6 ,7] ,[4 ,2 ,0 ,1] ,[5]]
+        , [[9 ,8 ,5 ,1] ,[2] ,[4 ,3] ,[6 ,0 ,7]]
+        , [[9 ,8 ,5 ,1] ,[2] ,[3] ,[6 ,0 ,4 ,7]]
+        , [[9 ,8 ,5 ,1] ,[2] ,[7] ,[4 ,6 ,0 ,3]]
+        , [[8 ,9] ,[7] ,[6 ,0 ,1] ,[2 ,5 ,4 ,3]]
+        , [[8 ,9] ,[7] ,[5 ,6 ,3] ,[4 ,0 ,2 ,1]]
+        , [[8 ,9] ,[5] ,[4 ,7] ,[6 ,0 ,1] ,[3] ,[2]]
+        , [[3] ,[5] ,[6 ,0 ,7] ,[2] ,[4 ,1] ,[8 ,9]] ] , 0 , N ).
+
+x_generator4_loop( [] , C , C ).
+x_generator4_loop( [ T | TS ] , C , N ):-
+  generator4( T ),
+  C1 is C + 1 ,
+  x_generator4_loop( TS , C1 , N ).
+x_generator4_loop( [ _ | TS ] , C , N ) :-
+	x_generator4_loop( TS , C , N ).
+
+x_tester4( N ):-
+  x_tester4_loop(
+    [ [[8 ,2 ,7] ,[6 ,1] ,[5 ,3] ,[4 ,0 ,9]]
+    , [[8 ,2 ,7] ,[6 ,1] ,[4 ,0 ,9] ,[5 ,3]]
+    , [[8 ,2 ,7] ,[5 ,3] ,[6 ,1] ,[4 ,0 ,9]]
+    , [[8 ,2 ,7] ,[4 ,0 ,9] ,[6 ,1] ,[5 ,3]]
+    , [[6 ,1] ,[8 ,2 ,7] ,[4 ,0 ,9] ,[5 ,3]]
+    , [[6 ,1] ,[4 ,0 ,9] ,[5 ,3] ,[8 ,2 ,7]]
+    , [[5 ,3] ,[6 ,1] ,[4 ,0 ,9] ,[8 ,2 ,7]]
+    , [[5 ,3] ,[4 ,0 ,9] ,[6 ,1] ,[8 ,2 ,7]]
+    , [[4 ,0 ,9] ,[5 ,3] ,[8 ,2 ,7] ,[6 ,1]]
+    , [[4 ,0 ,9] ,[8 ,2 ,7] ,[6 ,1] ,[5 ,3]] ] , 0 , N ).
+
+x_tester4_loop( [] , C , C ).
+x_tester4_loop( [ T | TS ] , C , N ):-
+  	tester4( T ) ,
+  	C1 is C + 1 ,
+	x_tester4_loop( TS , C1 , N ).
+x_tester4_loop( [ _ | TS ] , C , N ):-
+	x_tester4_loop( TS , C , N ).
